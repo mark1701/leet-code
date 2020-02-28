3821148aee9982c67d02b0ae36a420b79378635d
marco <none@none.com>
2020-02-28 12:39:15 +0000

    added problem 990

---
+++ problems/990/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean equationsPossible(String[] equations) {
+        //parse input
+            //take every equation
+            //take symbol 1 and if we have a corresponding number use the number
+            //otherwise assigne a number asnd store the mapping 
+            //repeat for symbol 2
+        
+        //number of nodes == number of keys
+        //generate a graph using equations
+        
+        //run a dfs and calculate the cc of the graph
+        
+        //finally run through the equations and check that 
+            // == are in the same cc
+            // != are in different cc
+            //if at any time one constraint is violated return false
+        
+        //return true
+    }
+}
