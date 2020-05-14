32558a5ac19bc05213ca0588a19ec09f5738f51f
marco <marco.passuello@gmail.com>
2020-05-14 16:09:17 +0100

    added solution for 122

---
+++ problems/0122/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    int T_ik0 = 0, T_ik1 = Integer.MIN_VALUE;
+    
+    for (int price : prices) {
+        T_ik0 = Math.max(T_ik0, T_ik1 + price);
+        T_ik1 = Math.max(T_ik1, T_ik0 - price);
+    }
+    
+    return T_ik0;
+}
+}
